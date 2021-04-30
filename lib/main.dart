ec4f9663b84ed91a78772f875f175bdbc422f9d8
menaaziz27 <menaaziz27@gmail.com>
2021-04-30 07:30:38 +0200

    sounds good!

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
+
+void main() => runApp(XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SafeArea(
+          child: Center(
+            child: TextButton(onPressed: () {
+              final player = new AudioCache();
+              player.play("note1.wav");
+            },
+                child: Text("Click me")),
+          ),
+        ),
+      ),
+    );
+  }
+}
